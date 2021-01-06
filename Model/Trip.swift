2064eb9a3db48799ba47cb12ae15d3d38c2204a3
niktia <kot33adpot@gmail.com>
2021-01-06 13:33:54 -0500

    Model Added

---
+++ Model/Trip.swift
@@ -0,0 +1,18 @@
+//
+//  Trip.swift
+//  RideFlagChallenge
+//
+//  Created by Nikita on 2021-01-06.
+//
+
+import Foundation
+
+class Trip:NSObject{
+    var longitude:Double = 0.0
+    var latitude:Double = 0.0
+    var passengerMiles:Double = 0.0
+    var driverMiles:Double = 0.0
+    var tripId:Double = 0.0
+    var tripDirections:String = ""
+    var tripDate:Double = 0.0
+}
